7bb61afe9f6bbdb2b74e56e61afe939d60147d50
Rasmus Pedersen <18396856+rpede@users.noreply.github.com>
2024-08-15 19:40:00 +0200

    Fix build

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Link, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Root from "./Root";
 import About from "./About";
 
